0be75ec581db4e961761e7b065ad37691c61bbe5
VOLODYA_PC\Volodymyr <ramon2921@gmail.com>
2016-09-20 19:04:57 +0300

    with classes

---
+++ Lab1/Program/2dFigure/WindowsFormsApplication1/Line.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Line
+    {
+        public Line(int x1, int y1, int x2, int y2)
+        {
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+        }
+
+        public int X1 { get; set; }
+
+        public int Y1 { get; set; }
+
+        public int X2 { get; set; }
+
+        public int Y2 { get; set; }
+
+        public double K
+        {
+            get
+            {
+                if ((X2 == X1) && (Y1 != Y2))
+                    return 0;
+                return (Y2 - Y1) / (X2 - X1);
+            }
+        }
+
+        public double Angle => Math.Atan(K) * 180 / Math.PI;
+
+        public string Output()
+        {
+            double k = K, b = Y1 - k * X1;
+            return $"y = {(Y2 - Y1)/(X2 - X1)} * x {(b < 0 ? '-' : '+')} {Math.Abs(b)}";
+        }
+
+        public static bool operator |(Line left, Line right)
+        {
+            return Math.Abs(left.K - right.K) < 0.1;
+        }
+    }
+}
---
+++ Lab1/Program/2dFigure/WindowsFormsApplication1/Parallelogram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Parallelogram
+    {
+        public Parallelogram(Point vertexA, Point vertexB, Point vertexC, Point vertexD)
+        {
+            VertexA = vertexA;
+            VertexB = vertexB;
+            VertexC = vertexC;
+            VertexD = vertexD;
+        }
+
+        public Color DiagramColor { get; set; } = Color.BlueViolet;
+        public Color BackgroundColor { get; set; } = Color.Aquamarine;
+
+        public Point VertexA { get; set; }
+
+        public Point VertexB { get; set; }
+
+        public Point VertexC { get; set; }
+
+        public Point VertexD { get; set; }
+
+        public bool IsCorrect()
+        {
+            var l1 = new Line(VertexA.X, VertexA.Y, VertexB.X, VertexB.Y);
+            var l2 = new Line(VertexB.X, VertexB.Y, VertexC.X, VertexC.Y);
+            var l3 = new Line(VertexC.X, VertexC.Y, VertexD.X, VertexD.Y);
+            var l4 = new Line(VertexA.X, VertexA.Y, VertexD.X, VertexD.Y);
+            return l1|l3 && l2|l4;
+        }
+    }
+}
